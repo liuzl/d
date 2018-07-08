466a4d51b66110b0a277d4e7c86a6294edf567b9
liuzhanliang <zhanliangliu@qq.com>
2018-07-08 21:09:25 +0800

    10 -> 1000

--- sync.go
+++ sync.go
@@ -7,7 +7,7 @@ import (
 func (d *Dictionary) sync() {
 	for {
 		time.Sleep(5 * time.Second)
-		if d.changed > 10 || time.Now().Unix()-d.updated > 60*5 {
+		if d.changed > 1000 || time.Now().Unix()-d.updated > 60*5 {
 			d.Save()
 		}
 	}
