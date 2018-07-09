2c1fd6a4a5a6f07808f47d4d37185f55dece6507
liuzhanliang <zhanliangliu@qq.com>
2018-07-10 06:15:10 +0800

    add web.go

---
+++ web.go
@@ -0,0 +1,51 @@
+package d
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/liuzl/goutil/rest"
+)
+
+func (d *Dictionary) GetHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	word := strings.TrimSpace(r.FormValue("word"))
+	values, err := d.Get(word)
+	if err != nil {
+		rest.MustEncode(w, &rest.RestMessage{"ERROR", err.Error()})
+		return
+	}
+	rest.MustEncode(w, &rest.RestMessage{"OK", values})
+}
+
+func (d *Dictionary) PrefixMatchHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	text := strings.TrimSpace(r.FormValue("text"))
+	ret, err := d.PrefixMatch(text)
+	if err != nil {
+		rest.MustEncode(w, &rest.RestMessage{"ERROR", err.Error()})
+		return
+	}
+	rest.MustEncode(w, &rest.RestMessage{"OK", ret})
+}
+
+func (d *Dictionary) UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	replace := strings.TrimSpace(r.FormValue("replace"))
+	data := strings.TrimSpace(r.FormValue("json"))
+	var rec Record
+	if err := json.Unmarshal([]byte(data), &rec); err != nil {
+		rest.MustEncode(w, &rest.RestMessage{"ERROR", err.Error()})
+		return
+	}
+	f := d.Update
+	if replace != "" {
+		f = d.Replace
+	}
+	if err := f(rec.K, rec.V); err != nil {
+		rest.MustEncode(w, &rest.RestMessage{"ERROR", err.Error()})
+		return
+	}
+	rest.MustEncode(w, &rest.RestMessage{"OK", "done"})
+}
