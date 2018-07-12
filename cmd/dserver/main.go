bd46e67182bfc2930b9af86d00e7a23c10d09919
Zhanliang Liu <zhanliangliu@qq.com>
2018-07-12 09:44:51 +0800

    update dserver

--- cmd/dserver/main.go
+++ cmd/dserver/main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
 
+	"github.com/golang/glog"
 	"github.com/liuzl/d"
 )
 
 var (
-	dir = flag.String("dir", "data", "data dir")
+	dir  = flag.String("dir", "data", "data dir")
+	addr = flag.String("addr", ":8080", "band address")
 )
 
 func main() {
 	flag.Parse()
+	defer glog.Flush()
+
 	dict, err := d.Load(*dir)
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
-	log.Println(dict)
+
 	dict.RegisterWeb()
-	http.ListenAndServe(":8080", nil)
+
+	glog.Info("dserver listen on", *addr)
+	glog.Error(http.ListenAndServe(":8080", nil))
 }
